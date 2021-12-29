40fa16c2d085453ac06ce8fd9f71cc5a96be10da
eyabenaouicha <eyabenaouicha25@gmail.com>
2021-12-29 20:25:17 +0100

    aaawww

--- FrontEnd/lib/Screens/HomePage.dart
+++ FrontEnd/lib/Screens/HomePage.dart
@@ -193,39 +193,7 @@ class _HomePageState extends State<HomePage> {
 // ---------------------------------------------------------------
 
           SizedBox(height: 50),
-          PersistentTabView(
-      context,
-      controller: _controller,
-      screens: _buildScreens(),
-      items: _navBarsItems(),
-      confineInSafeArea: true,
-      backgroundColor: Color(0xffEFEFF3), // Default is Colors.white.
-      handleAndroidBackButtonPress: true, // Default is true.
-      resizeToAvoidBottomInset:
-          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-      stateManagement: true, // Default is true.
-      hideNavigationBarWhenKeyboardShows:
-          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-      decoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(30.0),
-        colorBehindNavBar: Colors.white,
-      ),
-      popAllScreensOnTapOfSelectedTab: true,
-      popActionScreens: PopActionScreensType.all,
-      itemAnimationProperties: ItemAnimationProperties(
-        // Navigation Bar's items animation properties.
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      ),
-      screenTransitionAnimation: ScreenTransitionAnimation(
-        // Screen transition animation on change of selected tab.
-        animateTabTransition: true,
-        curve: Curves.easeInOut,
-        duration: Duration(milliseconds: 300),
-      ),
-      navBarStyle:
-          NavBarStyle.style12, // Choose the nav bar style with this property.
-    ),
+         
             ],
           ),
         ),
@@ -233,41 +201,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
   
-  List<Widget> _buildScreens() {
-    return [
-      HomePage(),
-      BookedPage(),
-      NotificationPage(),
-      Dashresponsable(),
-    ];
+
+    
   }
 
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.home),
-        title: ("Home"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.bookmark_fill),
-        title: ("Booked"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.bell_fill),
-        title: ("Notifications"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.person_alt_circle),
-        title: ("User"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-    ];
-  }
-}
